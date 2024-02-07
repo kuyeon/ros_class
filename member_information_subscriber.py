92175fca18f8e6ed939eddac5b1c5567252743bf
Park Kuyeon <53827090+kuyeon@users.noreply.github.com>
2024-02-08 04:24:14 +0900

    Create member_information_subscriber.py

---
+++ member_information_subscriber.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import rospy
+from my_first_package.msg import MemberInfo
+
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data)
+
+def member_information_subscriber():
+    rospy.init_node('member_information_subscriber', anonymous=True)
+
+    rospy.Subscriber('member_info', MemberInfo, callback)
+    rospy.spin()
+
+if __name__ == '__main__':
+    member_information_subscriber()
