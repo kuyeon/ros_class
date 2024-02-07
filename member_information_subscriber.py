cbce49535e7fef9e314625259ba64f768314fbd5
Park Kuyeon <53827090+kuyeon@users.noreply.github.com>
2024-02-08 04:37:48 +0900

    Update member_information_subscriber.py

--- member_information_subscriber.py
+++ member_information_subscriber.py
@@ -6,7 +6,7 @@ def callback(data):
     rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data)
 
 def member_information_subscriber():
-    rospy.init_node('member_information_subscriber', anonymous=True)
+    rospy.init_node('member_information_subscriber', anonymous=False)
 
     rospy.Subscriber('member_info', MemberInfo, callback)
     rospy.spin()
